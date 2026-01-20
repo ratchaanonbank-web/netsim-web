ef3fb74823e2450a38d90e9ee3f4d367d76106fc
ratchaanonbank-web <ratchaanonbank@gmail.com>
2026-01-20 20:50:47 +0700

    Update homepro.js

--- homepro.js
+++ homepro.js
@@ -4,13 +4,13 @@ const supabaseUrl = "https://mdwdzmkgehxwqotczmhh.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kd2R6bWtnZWh4d3FvdGN6bWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0ODg5MjksImV4cCI6MjA4MjA2NDkyOX0.lthMFiCQjq6ufGBkk0qs3nET6V3WTdprIZZQ4hM4R6M";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const userId = localStorage.getItem("userid");
+const id = localStorage.getItem("id");
 
 const imgEl = document.getElementById("profileImage");
 const nameEl = document.getElementById("profileName");
 const emailEl = document.getElementById("profileEmail");
 
-if (!userId) {
+if (!id) {
     document.querySelector(".profile-btn").style.display = "none";
 } else {
     loadProfile();
@@ -20,7 +20,7 @@ async function loadProfile() {
     const { data, error } = await supabase
         .from("users")
         .select("name, surname, email, imgprofile")
-        .eq("userid", userId)
+        .eq("id", id)
         .single();
 
     if (error) {
