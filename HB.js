88e873a7bd73e01aa4993a8ba541414672e7df33
ratchaanonbank-web <ratchaanonbank@gmail.com>
2026-01-20 08:38:46 +0700

    Update HB.js

--- HB.js
+++ HB.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "450px";
+  document.getElementById("mySidepanel").style.width = "350px";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
